0b534a1c6e0d110b523ad901d40b5560e69e5784
Weston Ruter <weston@x-team.com>
2013-07-16 11:37:01 -0700

    Fix PHP notice
    Notice: Undefined index: title in /srv/www/wordpress-default/wp-content/plugins/click_points_plugin/widget.php on line 169

--- widget.php
+++ widget.php
@@ -166,15 +166,20 @@ class cpjr3_widget extends WP_Widget {
 
 	function form( $instance ) {
 
-		$title = esc_attr( $instance['title'] );
+		$instance = array_merge(
+			array(
+				'title' => '',
+			),
+			$instance
+		);
 
 		?>
 
 		<p>
 
 			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:' ); ?></label>
-  
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
+
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" />
 
 		</p>
 
